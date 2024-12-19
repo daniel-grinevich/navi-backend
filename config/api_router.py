35bee24ad5c60705246378dec69fabf6804eba06
daniel-grinevich <kgbdannn@gmail.com>
2024-12-18 18:03:13 -0600

    add make commands and pushed fakeapi

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from navi_backend.fakeapi.api.views import FakeApiViewSet
 from navi_backend.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("fakeapi", FakeApiViewSet)
 
 
 app_name = "api"
