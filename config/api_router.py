9eb11dce004b50cd0ff9d8359ffc7129a1c87a7b
daniel-grinevich <kgbdannn@gmail.com>
2024-12-27 17:33:11 -0500

    it runs

--- config/api_router.py
+++ config/api_router.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
-from navi_backend.fakeapi.api.views import OptionSetsViewSet
+# from navi_backend.fakeapi.api.views import OptionSetsViewSet
 from navi_backend.fakeapi.api.views import OptionViewSet
 from navi_backend.fakeapi.api.views import ProductViewSet
 
@@ -12,9 +12,9 @@ from navi_backend.users.api.views import UserViewSet
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
-router.register(r"products", ProductViewSet)
-router.register(r"options", OptionViewSet)
-router.register(r"optionsets", OptionSetsViewSet)
+router.register(r"products", ProductViewSet, basename="products")
+router.register(r"options", OptionViewSet, basename="options")
+# router.register(r"optionsets", OptionSetsViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
---
+++ navi_backend/core/validators.py
@@ -0,0 +1,6 @@
+def validate_image_size(input):
+    print(input)
+
+
+def validate_image_extension(input):
+    print(input)
