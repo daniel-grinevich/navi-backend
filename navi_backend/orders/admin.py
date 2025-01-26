71c63b1f2646da67547a505279f1cf9bd01e0e20
jaridgeway <jamesaridgeway@gmail.com>
2025-01-26 11:57:47 -0600

    adding orders routes

--- navi_backend/orders/admin.py
+++ navi_backend/orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import (
+    MenuItem,
+    Order,
+    OrderItem,
+    OrderCustomization,
+    Category,
+    PaymentType,
+    Port,
+    CustomizationGroup,
+)
 
 # Register your models here.
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(OrderCustomization)
+admin.site.register(Category)
+admin.site.register(PaymentType)
+admin.site.register(Port)
+admin.site.register(CustomizationGroup)
