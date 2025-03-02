8dc8ba6a4c2fa9a0b3bcfa4618425b152196aaf6
jaridgeway <jamesaridgeway@gmail.com>
2025-03-02 16:14:09 -0600

    more tests

--- navi_backend/orders/tests/test_drf_views.py
+++ navi_backend/orders/tests/test_drf_views.py
@@ -9,7 +9,8 @@ from navi_backend.orders.models import (
     Port,
     MenuItem,
 )
-from .conftest import new_order, new_order_item, new_menu_item, admin_user
+from .factories import OrderFactory
+from navi_backend.orders.tests.factories import UserFactory
 from navi_backend.users.models import User
 from navi_backend.orders.api.views import (
     MenuItemViewSet,
@@ -17,19 +18,71 @@ from navi_backend.orders.api.views import (
     OrderViewSet,
 )
 
+# Potentially switch request setup to generic function
 
+
+@pytest.mark.django_db
 class TestOrderViewSet:
-    @pytest.fixture
-    def api_rf(self) -> APIRequestFactory:
-        return APIRequestFactory()
 
-    def test_get_queryset(self, admin_user, api_rf: APIRequestFactory):
-        view = OrderViewSet.as_view({"get": "list"})
-        request = api_rf.get("/api/orders/")
-        request.user = admin_user
+    @pytest.mark.parametrize(
+        "view_name, method, url",
+        [
+            ("list", "GET", "/api/orders/"),
+            ("retrieve", "GET", "/orders/{pk}/"),
+        ],
+    )
+    def test_not_admin_access(
+        self, user_and_orders, get_response, view_name, method, url
+    ):
+        """Non-admin users should only see their own orders."""
+        user, own_order, other_order = user_and_orders
+        view = OrderViewSet.as_view({method.lower(): view_name})
 
-        response = view(request)
+        # List View
+        if view_name == "list":
+            response = get_response(user, view, method, url)
+            order_ids = [order["slug"] for order in response.data]
+            assert str(own_order.slug) in order_ids
+            assert str(other_order.slug) not in order_ids
 
-        assert response.status_code == status.HTTP_200_OK
+        # Retrieve View
+        if view_name == "retrieve":
+            response = get_response(
+                user, view, method, url.format(pk=own_order.pk), pk=own_order.pk
+            )
+            assert response.status_code == status.HTTP_200_OK
 
-    # Add test to make sure Admin can see all orders and normal users can only see their orders
+            response = get_response(
+                user, view, method, url.format(pk=other_order.pk), pk=other_order.pk
+            )
+            assert response.status_code in [
+                status.HTTP_403_FORBIDDEN,
+                status.HTTP_404_NOT_FOUND,
+            ]
+
+    @pytest.mark.parametrize(
+        "view_name, method, url",
+        [
+            ("list", "GET", "/api/orders/"),
+            ("retrieve", "GET", "/orders/{pk}/"),
+        ],
+    )
+    def test_admin_access(self, admin_and_orders, get_response, view_name, method, url):
+        """Admin users should have access to all orders."""
+        admin, orders = admin_and_orders
+        view = OrderViewSet.as_view({method.lower(): view_name})
+
+        # List View
+        if view_name == "list":
+            response = get_response(admin, view, method, url)
+            order_ids = [order["slug"] for order in response.data]
+            assert len(order_ids) == len(orders)
+            assert all(str(order.slug) in order_ids for order in orders)
+
+        # Retrieve View
+        if view_name == "retrieve":
+            for order in orders:
+                response = get_response(
+                    admin, view, method, url.format(pk=order.pk), pk=order.pk
+                )
+                assert response.status_code == status.HTTP_200_OK
