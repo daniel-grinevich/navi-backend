f1a79df531c6dfda5dca6b4e4ae9bd79882790fd
daniel-grinevich <kgbdannn@gmail.com>
2025-04-07 21:01:21 -0500

    celery working in staging

--- config/celery.py
+++ config/celery.py
@@ -1,10 +1,6 @@
-import os
 from celery import Celery
 from django.conf import settings
 
-
-# Set the default Django settings module
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 app = Celery("navi_backend")
 
