fee4a25077b482edd9e2cf3f08eb20493d379b31
daniel-grinevich <kgbdannn@gmail.com>
2025-12-14 15:10:05 -0600

    push up local cors settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -86,11 +86,11 @@ CSRF_COOKIE_SECURE = False
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:5173",
     "http://127.0.0.1:3000",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:5173",
     "http://127.0.0.1:3000",
 ]
 
